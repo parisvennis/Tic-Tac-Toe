fe9ca4e7bf4cedfe22bcc1f317e5b212c8202043
parisvennis <parisvennis@gmail.com>
2020-07-31 13:38:46 +0000

    new updates

--- src/js/view/home.js
+++ src/js/view/home.js
@@ -7,7 +7,7 @@ export class Home extends React.Component {
 		this.state = {
 			player: null,
 			square: null,
-			lastValueToUser: "O",
+			nextUserInput: "X",
 			cell1: "",
 			cell2: "",
 			cell3: "",
@@ -21,42 +21,68 @@ export class Home extends React.Component {
 	}
 
 	handleClick = cellId => {
-		if (cellId === 1) this.setState({ cell1: "x" });
+		const newState = {};
+		newState[cellId] = this.state.nextUserInput;
+
+		if (this.state[cellId] === "") {
+			this.setState(newState);
+			this.setState({
+				nextUserInput: this.state.nextUserInput === "X" ? "O" : "X"
+			});
+		}
 	};
 
 	render() {
 		return (
 			<div className="text-center mt-5">
 				<div className="game-row">
-					<div className="cell" onClick={() => this.handleClick(1)}>
+					<div
+						className="cell"
+						onClick={() => this.handleClick("cell1")}>
 						{this.state.cell1}
 					</div>
-					<div className="cell" onClick={() => this.handleClick(2)}>
+					<div
+						className="cell"
+						onClick={() => this.handleClick("cell2")}>
 						{this.state.cell2}
 					</div>
-					<div className="cell" onClick={() => this.handleClick(3)}>
+					<div
+						className="cell"
+						onClick={() => this.handleClick("cell3")}>
 						{this.state.cell3}
 					</div>
 				</div>
 				<div className="game-row">
-					<div className="cell" onClick={() => this.handleClick(4)}>
+					<div
+						className="cell"
+						onClick={() => this.handleClick("cell4")}>
 						{this.state.cell4}
 					</div>
-					<div className="cell" onClick={() => this.handleClick(5)}>
+					<div
+						className="cell"
+						onClick={() => this.handleClick("cell5")}>
 						{this.state.cell5}
 					</div>
-					<div className="cell" onClick={() => this.handleClick(6)}>
+					<div
+						className="cell"
+						onClick={() => this.handleClick("cell6")}>
 						{this.state.cell6}
 					</div>
 				</div>
 				<div className="game-row">
-					<div className="cell" onClick={() => this.handleClick(7)}>
+					<div
+						className="cell"
+						onClick={() => this.handleClick("cell7")}>
 						{this.state.cell7}
 					</div>
-					<div className="cell" onClick={() => this.handleClick(8)}>
+					<div
+						className="cell"
+						onClick={() => this.handleClick("cell8")}>
 						{this.state.cell8}
 					</div>
-					<div className="cell" onClick={() => this.handleClick(9)}>
+					<div
+						className="cell"
+						onClick={() => this.handleClick("cell9")}>
 						{this.state.cell9}
 					</div>
 				</div>
